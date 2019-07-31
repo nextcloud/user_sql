f9e884c35b62436bae2ac104222bf518c8a8d885
Guillaume <g.h01@hotmail.com>
2019-07-31 13:53:38 +0200

    Update UserBackend.php

--- lib/Backend/UserBackend.php
+++ lib/Backend/UserBackend.php
@@ -446,13 +446,13 @@ final class UserBackend extends ABackend implements
             $this->cache->set("user_" . $user->uid, $user);
         }
 
-        $usersCache = array_map(
+        $users = array_map(
             function ($user) {
                 return $user->uid;
             }, $users
         );
 
-        $this->cache->set($cacheKey, $usersCache);
+        $this->cache->set($cacheKey, $users);
         $this->logger->debug(
             "Returning getUsers($search, $limit, $offset): count(" . count(
                 $users
